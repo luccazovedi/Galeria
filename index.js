dbd359825ee84c9784fb825f468696662c8e2828
Lucca Zovedi <80693993+luccazovedi@users.noreply.github.com>
2023-03-29 17:12:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,3 @@ import './style.css';
 
 const appDiv = document.getElementById('app');
 
-const galeria = document.getElementById('galeria');
-const quantidadeImagensIniciais = 1;
-let controle = 0;
-const carregarMais = document.getElementById('carregar-mais');
-for (let i = 0; i < quantidadeImagensIniciais; i++) {
-  galeria.innerHTML += `<img src="https://picsum.photos/400/250?random=+i+" />`;
-}
-carregarMais.addEventListener('click', () => {
-  for (let i = 0; i < quantidadeImagensIniciais; i++) {
-    const image = document.createElement('img');
-    image.src = `https://picsum.photos/400/250?random=${controle}`;
-    carregarMais.innerHTML = 'carregando...';
-    image.addEventListener('load', () => {
-      carregarMais.innerHTML = 'Carregar Mais!';
-    });
-    galeria.appendChild(image);
-    controle++;
-  }
-});
